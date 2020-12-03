af19cd6492f1f9912d662cd3cb8430dd701e7540
ademoldovan <75434459+ademoldovan@users.noreply.github.com>
2020-12-03 22:16:38 +0200

    Update drawingRobot.ino

--- drawingRobot.ino
+++ drawingRobot.ino
@@ -1,9 +1,10 @@
 
 #include <Servo.h>
-
 //unghiurile pentru servo motor atunci cand pixul este ridicat sau coborat
 int PEN_DOWN = 15;
 int PEN_UP = 80;
+//numarul pinului digital unde va fi atasat semnalul pentru motorul servo
+int servoPin = 8;
 Servo srv;
 
 
@@ -17,7 +18,7 @@ void loop() {
 
 }
 
-
+//functii pentru directiile in care sa mearga robotul
 void forward(float distance){
   
 }
@@ -37,7 +38,8 @@ void left(float degrees){
      
 }
 
-
+//functii pentru ridicat sau coborat pixul
+//servo motorul se va invarti un anumit unghi astfel incat va ridica/cobora pixul
 void penup(){
   delay(250);
   penServo.write(PEN_UP);
